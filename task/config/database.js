336a3195257aaec46de61ed7bce21c1908a49079
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2022-07-24 09:49:33 +0530

    inserting db url

--- task/config/database.js
+++ task/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Connection = async () => {
   try {
-    await mongoose.connect(process.env.db, { useNewUrlParser: true });
+    await mongoose.connect("mongodb+srv://mern:mern@cluster0.o56f2.mongodb.net/ecommercetask?retryWrites=true&w=majority", { useNewUrlParser: true });
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting with the database ", error);
